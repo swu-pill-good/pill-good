4d136e6f93a2f5b0ba832adb25524146b5262c88
yjsprout <ilbnmyj23@gmail.com>
2023-06-30 21:51:27 +0900

    yjpractice12

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {//,,
+            MyApplicationTheme {//,,,
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),//k
