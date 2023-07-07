22e87d07d8b265f007f10afb195b134104c6d19e
yjsprout <ilbnmyj23@gmail.com>
2023-07-07 13:11:11 +0900

    base

--- app/src/main/java/com/example/myapplication/MedMain.kt
+++ app/src/main/java/com/example/myapplication/MedMain.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MedMain : AppCompatActivity() {
+    //lateinit var
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_med_main)
