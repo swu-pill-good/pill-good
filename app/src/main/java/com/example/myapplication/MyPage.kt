513ce62c8034607aa17d1666d8feca8693ea8711
qhdus15 <song010912@naver.com>
2023-07-11 16:42:56 +0900

    qhdus

---
+++ app/src/main/java/com/example/myapplication/MyPage.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_page)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+
+//    override fun onSupportNavigateUp(): Boolean {
+//        onBackPressed()
+//        return true
+//    }
+}
