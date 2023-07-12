8dbdfa1e9f499302138a6bb71ca3a914ba1df617
owner <https://github.com/parkshun/GURU2.git>
2023-07-12 19:12:59 +0900

    pys

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.activity.ComponentActivity
+
 /*import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -15,27 +16,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme*/
 
 class MainActivity : ComponentActivity() {
-    lateinit var main_QR_button: ImageView
-    lateinit var main_list_button: ImageView
-    lateinit var main_my_button: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        main_QR_button=findViewById(R.id.main_QR_button)
-        main_list_button=findViewById(R.id.main_list_button)
-        main_my_button=findViewById(R.id.main_my_button)
 
-        main_QR_button.setOnClickListener {
-            var intent= Intent(this,QR::class.java)
-            startActivity(intent)
-        }
-        main_list_button.setOnClickListener {
-            var intent= Intent(this,MedMain::class.java)
-            startActivity(intent)
-        }
-        main_my_button.setOnClickListener {
-            var intent= Intent(this,MyPage::class.java)
-            startActivity(intent)
-        }
     }
 }
