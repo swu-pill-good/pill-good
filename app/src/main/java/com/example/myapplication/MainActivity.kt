729dc485e50ce1cdaf7d49329a758fd8f6044360
owner <https://github.com/parkshun/GURU2.git>
2023-07-16 02:07:56 +0900

    pys

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.activity.ComponentActivity
 
@@ -16,10 +17,18 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme*/
 
 class MainActivity : ComponentActivity() {
+    lateinit var main_QR_button:ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        main_QR_button=findViewById(R.id.main_QR_button)
+
+        main_QR_button.setOnClickListener{
+            var intent= Intent(this,QR::class.java)
+            startActivity(intent)
+
+        }
 
 
 
